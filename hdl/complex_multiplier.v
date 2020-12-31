a49bbeec4136ccff3b456d24ea5acc3b1d56f7c8
Benjamin Menkuec <benjamin@menkuec.de>
2020-12-31 15:45:43 +0100

    passes unit test

--- hdl/complex_multiplier.v
+++ hdl/complex_multiplier.v
@@ -1,67 +1,67 @@
 module complex_multiplier
-	#(parameter	INPUT_WIDTH_A = 16,
-	  parameter	INPUT_WIDTH_B = 16,
-	  parameter	OUTPUT_WIDTH = 32,
-	  parameter TRUNCATE = 1)
-	( 	
-		input wire			   clk, reset,
-		// slave a
-		input      signed [INPUT_WIDTH_A-1:0] s_axis_a_tdata,
-		output wire							  s_axis_a_tready,
-		input wire							  s_axis_a_tvalid,
-		// slave b
-		input	   signed [INPUT_WIDTH_B-1:0] s_axis_b_tdata,
-		output wire							  s_axis_b_tready,
-		input wire							  s_axis_b_tvalid,
-		// master output
-		output reg signed [OUTPUT_WIDTH-1:0] m_axis_tdata,
-		output wire							 m_axis_tvalid,
-		input wire							 m_axis_tready
-		);
-	// p = a*b = p_r + jp_i = (a_r*b_r - a_i*b_i) + j(a_r*b_i + a_i*b_r)
-	// stage1: calculate a_r*b_r, a_i*b_i, a_r*b_i, a_i*b_r
-	// stage2: calculate p_r and p_i
+    #(parameter INPUT_WIDTH_A = 16,
+      parameter INPUT_WIDTH_B = 16,
+      parameter OUTPUT_WIDTH = 32,
+      parameter TRUNCATE = 1)
+    (   
+        input wire             clk, rst,
+        // slave a
+        input             [INPUT_WIDTH_A-1:0] s_axis_a_tdata,
+        output wire                           s_axis_a_tready,
+        input wire                            s_axis_a_tvalid,
+        // slave b
+        input             [INPUT_WIDTH_B-1:0] s_axis_b_tdata,
+        output wire                           s_axis_b_tready,
+        input wire                            s_axis_b_tvalid,
+        // master output
+        output reg 		  [OUTPUT_WIDTH-1:0] m_axis_tdata,
+        output wire                          m_axis_tvalid,
+        input wire                           m_axis_tready
+        );
+    // p = a*b = p_r + jp_i = (a_r*b_r - a_i*b_i) + j(a_r*b_i + a_i*b_r)
+    // stage1: calculate a_r*b_r, a_i*b_i, a_r*b_i, a_i*b_r
+    // stage2: calculate p_r and p_i
 
-	reg signed [OUTPUT_WIDTH/2-1:0]	ar_br, ai_bi, ar_bi, ai_br;
+    reg [OUTPUT_WIDTH/2-1:0] ar_br, ai_bi, ar_bi, ai_br;
 
-	wire [INPUT_WIDTH_A/2-1:0] a_r;
-	wire [INPUT_WIDTH_A/2-1:0] a_i;
-	wire [INPUT_WIDTH_B/2-1:0] b_r;
-	wire [INPUT_WIDTH_B/2-1:0] b_i;
-	assign a_r = s_axis_a_tdata[INPUT_WIDTH_A-1:INPUT_WIDTH_A/2];
-	assign a_i = s_axis_a_tdata[INPUT_WIDTH_A/2-1:0];
-	assign b_r = s_axis_b_tdata[INPUT_WIDTH_B-1:INPUT_WIDTH_B/2];
-	assign b_i = s_axis_b_tdata[INPUT_WIDTH_B/2-1:0];
-	assign s_axis_a_tready = 1;
-	assign s_axis_b_tready = 1;
-	assign m_axis_tvalid = 1;
-	
-	localparam TRUNC_BITS = INPUT_WIDTH_A + INPUT_WIDTH_B - OUTPUT_WIDTH;
+    wire [INPUT_WIDTH_A/2-1:0] a_r;
+    wire [INPUT_WIDTH_A/2-1:0] a_i;
+    wire [INPUT_WIDTH_B/2-1:0] b_r;
+    wire [INPUT_WIDTH_B/2-1:0] b_i;
+    assign a_i = s_axis_a_tdata[INPUT_WIDTH_A-1:INPUT_WIDTH_A/2];
+    assign a_r = s_axis_a_tdata[INPUT_WIDTH_A/2-1:0];
+    assign b_i = s_axis_b_tdata[INPUT_WIDTH_B-1:INPUT_WIDTH_B/2];
+    assign b_r = s_axis_b_tdata[INPUT_WIDTH_B/2-1:0];
+    assign s_axis_a_tready = 1;
+    assign s_axis_b_tready = 1;
+    assign m_axis_tvalid = 1;
+    
+    localparam TRUNC_BITS = INPUT_WIDTH_A + INPUT_WIDTH_B - OUTPUT_WIDTH;
 
-	always @(posedge clk) begin
-		if (~reset) begin
-			m_axis_tdata <= {(OUTPUT_WIDTH){1'b0}};
-		end
-		else begin
-			if (TRUNC_BITS != 0) begin
-			// no rounding or truncation needed
-				ar_br <= a_r * b_r;
-				ai_bi <= a_i * b_i;
-				ar_bi <= a_r * b_i;
-				ai_br <= a_i * b_r;
-			end
-			else begin
-				if (TRUNCATE == 1) begin
-					ar_br <= ((a_r * b_r)>>TRUNC_BITS);
-					ai_bi <= ((a_i * b_i)>>TRUNC_BITS);
-					ar_bi <= ((a_r * b_i)>>TRUNC_BITS);
-					ai_br <= ((a_i * b_r)>>TRUNC_BITS);					
-				end
-				else begin
-				// TODO: implement rounding
-				end
-			end
-			m_axis_tdata <= {{ar_br - ai_bi},{ar_bi + ai_br}};
-		end
-	end
+    always @(posedge clk) begin
+        if (~rst) begin
+            m_axis_tdata <= {(OUTPUT_WIDTH){1'b0}};
+        end
+        else begin
+            if (TRUNC_BITS != 0) begin
+            // no rounding or truncation needed
+                ar_br <= a_r * b_r;
+                ai_bi <= a_i * b_i;
+                ar_bi <= a_r * b_i;
+                ai_br <= a_i * b_r;
+            end
+            else begin
+                if (TRUNCATE == 1) begin
+                    ar_br <= ((a_r * b_r)>>TRUNC_BITS);
+                    ai_bi <= ((a_i * b_i)>>TRUNC_BITS);
+                    ar_bi <= ((a_r * b_i)>>TRUNC_BITS);
+                    ai_br <= ((a_i * b_r)>>TRUNC_BITS);                 
+                end
+                else begin
+                // TODO: implement rounding
+                end
+            end
+            m_axis_tdata <= {{ar_br - ai_bi},{ar_bi + ai_br}};
+        end
+    end
 endmodule
--- tests/test_complex_multiplier.py
+++ tests/test_complex_multiplier.py
@@ -1,6 +1,8 @@
 import cocotb
 from cocotb.clock import Clock
+from cocotb.triggers import Timer
 from cocotb.triggers import RisingEdge, ReadOnly
+from fixedpoint import FixedPoint
 
 import random
 import warnings
@@ -8,31 +10,57 @@ import warnings
 CLK_PERIOD_NS = 100
 
 
-class TB(object):
-    def __init__(self, dut):
-        self.dut = dut
+def setup_dut(dut):
+    cocotb.fork(Clock(dut.clk, CLK_PERIOD_NS, units='ns').start())
 
-        s_count = int(os.getenv("PARAM_S_COUNT"))
-        m_count = int(os.getenv("PARAM_M_COUNT"))
+# Test single multiplication
+@cocotb.test()
+async def single_multiplication_0(dut):
+    """Perform a single multiplication of two complex numbers
 
-        self.log = logging.getLogger("cocotb.tb")
-        self.log.setLevel(logging.DEBUG)
+    Test ID: 0
 
-        cocotb.fork(Clock(dut.clk, 10, units="ns").start())
+    Expected Results:
+        a*b = (a_r*b_r - a_i*b_i) + j(a_r*b_i + a_i*b_r)
+    """
 
-    async def cycle_reset(self):
-        self.dut.rst.setimmediatevalue(0)
-        await RisingEdge(self.dut.clk)
-        await RisingEdge(self.dut.clk)
-        self.dut.rst <= 1
-        await RisingEdge(self.dut.clk)
-        await RisingEdge(self.dut.clk)
-        self.dut.rst <= 0
-        await RisingEdge(self.dut.clk)
-        await RisingEdge(self.dut.clk)
-		
-		
-async def run_test_single_mult(dut):
-	while True:
-		return
+    # Reset
+    dut.rst <= 1
+    setup_dut(dut)
+    await Timer(CLK_PERIOD_NS * 1, units='ns')
+    dut.rst <= 0
+    await Timer(CLK_PERIOD_NS * 2, units='ns')
+    dut.rst <= 1
+    await Timer(CLK_PERIOD_NS * 2, units='ns')
+	
+    input_width_a = 16
+    input_width_b = 16
+    output_width = 32
+
+    a_r = FixedPoint(0x10, signed=True,m=input_width_a/2)
+    a_i = FixedPoint(0x20, signed=True,m=input_width_a/2)
+    b_r = FixedPoint(2, signed=True,m=input_width_a/2)
+    b_i = FixedPoint(3, signed=True,m=input_width_a/2)
+    numStages = 3
+
+    a_i.resize(input_width_a,0)
+    dut.s_axis_a_tdata <= int((a_i << int(input_width_a/2)) + a_r)
+    b_i.resize(input_width_a,0)
+    dut.s_axis_b_tdata <= int((b_i << int(input_width_b/2)) + b_r)
+    await Timer(CLK_PERIOD_NS * numStages, units='ns')
+
+    receivedValue = int(dut.m_axis_tdata).to_bytes(length=int(output_width/8),byteorder='big',signed=False)
+    received_i = int.from_bytes(receivedValue[int(len(receivedValue)/2):len(receivedValue)], byteorder='big', signed=True)
+    received_r = int.from_bytes(receivedValue[0:int(len(receivedValue)/2)],byteorder='big',signed=True)
+
+    correctResult = (FixedPoint(a_r*b_r - a_i*b_i,True,output_width)<<int(output_width/2)) + FixedPoint(a_r*b_r - a_i*b_i,True,output_width)
+    await Timer(CLK_PERIOD_NS * 2, units='ns')
+    assert received_r == a_r*b_r - a_i*b_i, ("(%i + j%i) * (%i + j%i), real part should have been "
+                           "%i but was %i " % (a_r,a_i,b_r,b_i,
+                           a_r*b_r - a_i*b_i,received_r))
+    assert received_i == a_r*b_i + a_i*b_r, ("(%i + j%i) * (%i + j%i), imaginary part should have been "
+                           "%i but was %i " % (a_r,a_i,b_r,b_i,
+                           a_r*b_i + a_i*b_r,received_i))
+    #dut._log.info("0x%08X * 0x%08X" % (A, B))
+
 	
